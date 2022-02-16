271c529ae223a382228ee83454f4b80e2f4bac41
Daniel <dsgonzalez@sbgtv.com>
2022-02-16 09:42:19 -0800

    clean

--- src/com/learnjava/Main.java
+++ src/com/learnjava/Main.java
@@ -11,37 +11,8 @@ import java.util.Scanner;
 public class Main {
     final static byte MONTHS_IN_YEAR = 12;
     final static byte PERCENT = 100;
+
     public static void main(String[] args) {
-
-        //Primitive types, byte 1, short 2, int 2, long 8, float 4, double 8, char 2, boolean True/False
-        byte myAge = 30;
-        long viewCount = 3_123_456_789L; //add sufix L to declare Long
-        float price = 10.99F;//add sufix to declare as float
-        char letter = 'A';//Single quotes for char
-        boolean isEligible = false;
-
-        //Reference  type
-        Date now = new Date();
-        System.out.println(now);
-
-        //reference type point to the address of that object, so it can overwrite memory
-        Point point = new Point(1,1);
-        Point point2 = point;
-        point.x = 2;
-        System.out.println(point2);
-
-        int [] numbers = new int[5];
-        numbers[0] = 1;
-        numbers[1] = 2;
-
-        System.out.println( Arrays.toString( numbers ) );
-
-        //casting
-        //Implicit casting
-        //  byte > short > int . long > float > double
-        short x = 1;
-        int y = x + 2;
-
 
         System.out.println("******************* MORTGAGE CALCULATOR ***********************");
 
@@ -52,27 +23,7 @@ public class Main {
         displayMortgage(annual, principal, years);
         displayBalance( annual, principal, years);
 
-        System.out.println("************************** FIZZBUZZ ***************************");
-        final String FIZZ = "Fizz";
-        final String BUZZ = "Buzz";//
-        String fizzBuzz;
 
-        //ask for the number
-        Scanner scanner = new Scanner(System.in);
-        System.out.print(("Number: "));
-        int number = scanner.nextInt();
-
-
-        if(number % 5 == 0 && number % 3 == 0)
-            fizzBuzz = FIZZ+BUZZ;
-        else if(number % 5 == 0)
-            fizzBuzz = FIZZ;
-        else if(number % 3 == 0)
-            fizzBuzz = BUZZ;
-        else
-            fizzBuzz = Integer.toString(number);
-
-        System.out.println("Results: " +  fizzBuzz );
 
     }
 
